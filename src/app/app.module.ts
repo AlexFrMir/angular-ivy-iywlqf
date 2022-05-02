3ce2ef3f488fbf1bf21faaed498ebd1923a6d360
AlexFrMir <franc_@live.com.mx>
2022-05-02 02:21:25 -0500

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MDBBootstrapModule.forRoot()],
+  declarations: [AppComponent, HelloComponent, NavbarComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
